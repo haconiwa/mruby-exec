1ec368752fc04fb44b165a0972cd589239a633cc
Uchio KONDO <udzura@udzura.jp>
2016-06-09 16:38:48 +0900

    Accept arguments

--- src/mrb_exec.c
+++ src/mrb_exec.c
@@ -11,19 +11,44 @@
 #include <unistd.h>
 
 #include "mruby.h"
-#include "mruby/data.h"
+#include "mruby/string.h"
 #include "mrb_exec.h"
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
+#ifdef DEBUG
+#define _DEBUGP printf
+#else
+#define _DEBUGP 1 ? (void) 0 : printf
+#endif
+
 static mrb_value mrb_exec_do_exec(mrb_state *mrb, mrb_value self)
 {
-  char *newargv[] = { NULL, "-l", NULL };
-  char *newenviron[] = { NULL };
+  mrb_value *mrb_argv;
+  mrb_int argc;
+  char **argv;
+  int i, j;
+  mrb_get_args(mrb, "*", &mrb_argv, &argc);
+  if(argc < 1) {
+    return mrb_nil_value();
+  }
 
-  execve("/bin/bash", newargv, newenviron);
+  for(i = 0; i < argc; i++) {
+    *argv = mrb_str_to_cstr(mrb, mrb_argv[i]);
+    argv++;
+  }
+  *argv = NULL;
 
-  perror("execve");
+  // move to start of array
+  argv = argv - i;
+
+  for(j = 0; j < argc + 1; j++) {
+    _DEBUGP("[mruby-exec] argv(%i): %s\n", j, argv[j]);
+  }
+
+  execv(argv[0], argv);
+
+  perror("execv");
   return mrb_nil_value();
 }
 
@@ -32,7 +57,7 @@ void mrb_mruby_exec_gem_init(mrb_state *mrb)
     struct RClass *e;
     e = mrb_define_class(mrb, "Exec", mrb->object_class);
     mrb_define_class_method(mrb, e, "exec", mrb_exec_do_exec, MRB_ARGS_ANY());
-    mrb_define_class_method(mrb, e, "execve", mrb_exec_do_exec, MRB_ARGS_ANY());
+    mrb_define_class_method(mrb, e, "execv", mrb_exec_do_exec, MRB_ARGS_ANY());
     DONE;
 }
 
