84e74af9f4c5df2b7c8e311194249ae89b5e7618
matsumotory <matsumotory@pepabo.com>
2017-02-01 05:52:08 +0000

    Add test for sys_fail wrapper

--- test/mrb_exec.rb
+++ test/mrb_exec.rb
@@ -28,3 +28,16 @@ assert("Kernel#exec") do
   assert_true(p.is_a?(Integer))
   assert_true(status.success?)
 end
+
+assert("Kernel#exec sys fail") do
+  current_dir = File.expand_path("#{File.dirname(__FILE__)}/..")
+  raised_func_of_file = "#{current_dir}/src/mrb_exec.c:mrb_exec_sys_fail"
+  expected_error_message = "sys failed #{raised_func_of_file} errno: 2 message: No such file or directory mrbgem message: execv failed"
+
+  begin
+    # always failed
+    p = Process.fork { exec "/bin/bbaash" }
+  rescue => e
+    assert_equal(expected_error_message, e.to_s)
+  end
+end
